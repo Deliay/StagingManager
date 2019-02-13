b83ec9d40acb5618ac78ea87dd64b33660cd51aa
Deliay <admin@remiliascarlet.com>
2019-02-13 23:31:35 +0800

    Circle CI services implement

---
+++ Controllers/CircleCIController.cs
@@ -0,0 +1,31 @@
+﻿using CheckStaging.Models;
+using CheckStaging.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheckStaging.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CircleCIController : Controller
+    {
+        [HttpPost]
+        public IActionResult Post([FromBody] CircleCIWebhookPayload value)
+        {
+            CircleCICommandService.Instance.PassCircleCIWebhook(value.payload);
+            return Ok();
+        }
+
+        [HttpPost("bc")]
+        public IActionResult BearyChatPost([FromBody] Incoming value)
+        {
+            if (value.token != "11f76f998feac74f9bb6de5bab293ed1") return Forbid("Auth Fail");
+            if (value.channel_name != "CI挂没挂" && value.channel_name != "Staging占坑测试频道") return Forbid("Wrong channel");
+            if (value.user_name == null || value.user_name.Length == 0) return Forbid("User name error");
+            return Ok(CircleCICommandService.Instance.PassIncoming(value));
+        }
+    }
+}
---
+++ Models/CircleCIWebhookPayload.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CheckStaging.Models
+{
+    public struct CirclePR
+    {
+        public string head_sha { get; set; }
+        public string url { get; set; }
+    }
+
+    public struct CircleCIUser
+    {
+        public string login { get; set; }
+        public string avatar_url { get; set; }
+        public string name { get; set; }
+        public int id { get; set; }
+        public string vcs_type { get; set; }
+    }
+    public struct CircleCIWebhook
+    {
+        public string branch { get; set; }
+        public CircleCIUser user { get; set; }
+        public string status { get; set; }
+        public string subject { get; set; }
+        public CirclePR[] pull_requests { get; set; }
+        public string outcome { get; set; }
+        public int build_num { get; set; }
+        public string build_url { get; set; }
+    }
+    public struct CircleCIWebhookPayload
+    {
+        public CircleCIWebhook payload { get; set; }
+    }
+
+}
