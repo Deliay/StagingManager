6a6108b9d92f01042147c391508776c38ddd111b
Deliay <admin@remiliascarlet.com>
2019-02-15 15:23:17 +0800

    Complete Outgoing model

--- Models/Outgoing.cs
+++ Models/Outgoing.cs
@@ -5,8 +5,25 @@ using System.Threading.Tasks;
 
 namespace CheckStaging.Models
 {
+    public struct OutgoingPicture
+    {
+        public string url { get; set; }
+    }
+    public struct OutgoingAttachment
+    {
+        public string title { get; set; }
+        public string url { get; set; }
+        public string text { get; set; }
+        public string color { get; set; }
+        public OutgoingPicture[] images { get; set; }
+    }
+
     public struct Outgoing
     {
         public string text { get; set; }
+        public string user { get; set; }
+        public string channel { get; set; }
+        public string notification { get; set; }
+        public OutgoingAttachment[] attachments { get; set; }
     }
 }
