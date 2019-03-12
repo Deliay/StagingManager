b6492e79a8976ca0806b0555bef0dd9787e866e8
Deliay <admin@remiliascarlet.com>
2019-03-12 12:44:11 +0800

    Enhance channel error hint

--- Controllers/StagingController.cs
+++ Controllers/StagingController.cs
@@ -22,7 +22,7 @@ namespace CheckStaging.Controllers
             }
             if (value.channel_name != "Staging占坑测试频道" && value.channel_name != "staging" && value.channel_name != "倍洽小助手")
             {
-                return Forbid("Channel error");
+                return Forbid($"{value.channel_name} is not a valid channel");
             }
             if (value.user_name == null || value.user_name.Length == 0)
             {
